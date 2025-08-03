f29d9fe26e32a2858b1542791c03c626c6b0cab0
ayodeji oni <ayodejioni1505@gmail.com>
2025-08-03 18:48:29 +0100

    feat: add 'create a student' endpoint implementation

--- task_1/main.py
+++ task_1/main.py
@@ -1,13 +1,35 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, status
+
+from schema import StudentCreateModel, StudentModel, AllStudentModel
+from utils import add_student, get_student, get_all_students  # noqa
 
 
 # application's main entry point
 app = FastAPI()
 
 
-@app.post("/students/")
-async def create_a_student():
+@app.post("/students/",
+          status_code=status.HTTP_201_CREATED)
+async def create_a_student(student_data: StudentCreateModel):
     """create a student endpoint"""
+    student = student_data.model_dump()
+
+    student_name: str = student["name"]
+    student_subjects: dict = student["subjects"]
+
+    try:
+        new_student_data = add_student(student_name, student_subjects)
+        return new_student_data
+    except KeyError:
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail="Student data already exists!"
+        )
+    except Exception:
+        raise HTTPException(
+            status_code=404,
+            detail="Student data creation failed!"
+        )
 
 
 @app.get("/students/{name}")
