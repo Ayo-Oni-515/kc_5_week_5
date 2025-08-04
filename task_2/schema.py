fe06eda6e9037bca12486b8cb7de6c38275108ad
ayodeji oni <ayodejioni1505@gmail.com>
2025-08-04 14:32:47 +0100

    feat: add request/response models

---
+++ task_2/schema.py
@@ -0,0 +1,20 @@
+from pydantic import BaseModel
+
+
+class ProductModel(BaseModel):
+    name: str
+    price: float
+    qty: int
+
+
+class AllProductModel(BaseModel):
+    data: dict
+
+
+class AllCartModel(BaseModel):
+    data: dict
+
+
+class AddToCartModel(BaseModel):
+    product_id: str
+    qty: int
